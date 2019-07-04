b6c1e6be0a8f7002fb765e05fd7ca6f40bdef602
Charles Davis <calandavis1990@gmail.com>
2019-07-04 17:30:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ function logWhisper(string){
   console.log (string.toLowerCase())
 }
 function sayHiToGrandma(string){
-  if (string.toLowerCase())
+  var a = string
+  if (a.toLowerCase())
   return "I can\'t hear you!"
-  else if (string.toUpperCase())
+  else if (a.toUpperCase())
 return "YES INDEED!"
   
   
